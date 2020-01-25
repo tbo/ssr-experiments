eba9bb0e17cb5dabab5fe2115c0320a2c2e8a3b9
Thomas Bonk <thomas@bonk.dev>
2020-01-25 21:51:42 +0100

    Initial page setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,18 @@
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended"
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
   overrides: [
     {
+      files: ['*.ts', '*.tsx'],
       rules: {
-        "@typescript-eslint/explicit-function-return-type": "off"
-      }
-    }
-  ]
+        '@typescript-eslint/explicit-function-return-type': 'off',
+      },
+    },
+  ],
 };
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,25 @@
-import fastify from "fastify";
+import fastify from 'fastify';
+import pointOfView from 'point-of-view';
+import handlebars from 'handlebars';
+import fastifyStatic from 'fastify-static';
+import path from 'path';
 const app = fastify({ logger: true });
 
 app
-  .get("/", async (request, reply) => ({ hello: "world" }))
+  .register(pointOfView, {
+    engine: {
+      handlebars,
+    },
+    options: {
+      partials: {
+        header: '/src/templates/header.hbs',
+        footer: '/src/templates/footer.hbs',
+      },
+    },
+  })
+  .get('/', async (_request, reply) => reply.view('/src/templates/search.hbs'))
+  .get('/examples', async (_request, reply) => reply.view('/src/templates/examples.hbs'))
+  .register(fastifyStatic, { root: path.join(__dirname, '../assets') })
   .listen(3000)
   .catch(error => {
     app.log.error(error);
