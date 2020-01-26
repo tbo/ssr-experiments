12df2f45008265e81b17621a86ce437cc6fe7972
Thomas Bonk <thomas@bonk.dev>
2020-01-26 13:29:58 +0100

    Handle only relative links and add form support

--- src/hybrid.ts
+++ src/hybrid.ts
@@ -7,9 +7,11 @@ const fetchOptions: RequestInit = {
 
 const isLink = (node: Node): node is HTMLAnchorElement => node.nodeName === 'A';
 
+const isLocalLink = (node: Node): boolean => isLink(node) && !~node.href?.indexOf('://');
+
 const morphdomOptions = {
   onBeforeNodeAdded: (added: Node) => {
-    if (isLink(added) && added.href) {
+    if (isLocalLink(added)) {
       added.addEventListener('click', handleClick);
     }
     return added;
@@ -20,8 +22,13 @@ const morphdomOptions = {
 
 const handleTransition = async (targetUrl: string) => {
   document.body.classList.add('is-loading');
-  const response = await fetch(targetUrl, fetchOptions);
-  morphdom(document.documentElement, await response.text(), morphdomOptions);
+  try {
+    const response = await fetch(targetUrl, fetchOptions);
+    morphdom(document.documentElement, await response.text(), morphdomOptions);
+  } catch {
+    console.error(`Unable to resolve "${targetUrl}". Doing hard load instead...`);
+    window.location.href = targetUrl;
+  }
   document.body.classList.remove('is-loading');
 };
 
@@ -32,8 +39,17 @@ const handleClick = async (event: Event) => {
   window.history.pushState(null, document.title, targetUrl);
 };
 
+const handleSubmit = async (event: Event) => {
+  event.preventDefault();
+  const form = event.target as HTMLFormElement;
+  const query = new URLSearchParams(new FormData(form) as any).toString();
+  const targetUrl = form.action + query ? '?' + query : '';
+  await handleTransition(targetUrl);
+  window.history.pushState(null, document.title, targetUrl);
+};
+
 document.addEventListener('DOMContentLoaded', function() {
-  const links = document.querySelectorAll('a[href]');
-  links.forEach(link => link.addEventListener('click', handleClick));
+  document.querySelectorAll('a:not([href*="://"]').forEach(link => link.addEventListener('click', handleClick));
+  document.querySelectorAll('form:not([action*="://"]').forEach(link => link.addEventListener('submit', handleSubmit));
   window.onpopstate = (event: PopStateEvent) => handleTransition((event?.target as Window).location.href);
 });
