08d64b81e84a280a70edd1c7951d9a8d9a17c0e8
Thomas Bonk <thomas@bonk.dev>
2020-02-22 16:06:54 +0100

    Add caching support

--- src/hybrid.ts
+++ src/hybrid.ts
@@ -32,6 +32,9 @@ const isLocalLink = (node: Node): boolean => {
 const isTextInput = (node: Node): node is HTMLInputElement =>
   node.nodeName === 'INPUT' && ['email', 'text', 'password'].includes((node as HTMLInputElement).type);
 
+let cache: Cache;
+window.caches.open('hybrid').then(c => (cache = c));
+
 const morphdomOptions = {
   onBeforeElUpdated: (from: Node, to: Node) => {
     if (
@@ -48,10 +51,28 @@ const morphdomOptions = {
 
 let abortController: AbortController | null = null;
 
-const handleRespone = async (response: Response) => {
+const parser = new DOMParser();
+
+const handleRespone = (mode: 'cache' | 'network', cacheRace: AbortController) => async (
+  response: Response | undefined,
+) => {
+  if (!response) {
+    return;
+  }
   const text = await response.text();
   const start = performance.now();
   const doc = parser.parseFromString(text, 'text/html');
+  // The cache can be slower, than the network in some rare cases. We abort the
+  // rendering of cached responses in those situations to avoid overriding
+  // up-to-date responses with stale data.
+  if (cacheRace.signal.aborted) {
+    console.log('abort');
+    console.warn('Cached got aborted');
+    return;
+  }
+  if (mode === 'network') {
+    cacheRace.abort();
+  }
   switch (ENGINE) {
     case 'nanomorph':
       nanomorph(document.documentElement, doc.documentElement);
@@ -59,41 +80,42 @@ const handleRespone = async (response: Response) => {
     case 'morphdom':
       morphdom(document.documentElement, doc.documentElement, morphdomOptions);
       break;
-    case 'diffdom':
-      {
-        const diff = dd.diff(document.documentElement, doc.documentElement);
-        const startApply = performance.now();
-        dd.apply(document.documentElement, diff);
-        const endApply = performance.now();
-        console.info('Application took', endApply - startApply);
-      }
+    case 'diffdom': {
+      const diff = dd.diff(document.documentElement, doc.documentElement);
+      const startApply = performance.now();
+      dd.apply(document.documentElement, diff);
+      const endApply = performance.now();
+      console.info('Application took', endApply - startApply);
       break;
-    case 'snabbdom':
-      {
-        const newVDom = toVNode(doc.documentElement);
-        const startApply = performance.now();
-        patch(currentVDom, newVDom);
-        currentVDom = newVDom;
-        const endApply = performance.now();
-        console.info('Application took', endApply - startApply);
-      }
+    }
+    case 'snabbdom': {
+      const newVDom = toVNode(doc.documentElement);
+      const startApply = performance.now();
+      patch(currentVDom, newVDom);
+      currentVDom = newVDom;
+      const endApply = performance.now();
+      console.info('Application took', endApply - startApply);
       break;
+    }
   }
   const end = performance.now();
   console.info('Transition took', end - start);
+  return response.url;
 };
 
-const parser = new DOMParser();
 const handleTransition = async (targetUrl: string) => {
   abortController?.abort();
   abortController = new AbortController();
+  const cacheRace = new AbortController();
   document.body.classList.add('is-loading');
+  cache?.add(targetUrl);
+  cache?.match(targetUrl).then(handleRespone('cache', cacheRace));
   return fetch(targetUrl, {
     credentials: 'same-origin',
     redirect: 'follow',
     signal: abortController.signal,
   })
-    .then(handleRespone)
+    .then(handleRespone('network', cacheRace))
     .catch(error => {
       if (error.name !== 'AbortError') {
         console.error(error, `Unable to resolve "${targetUrl}". Doing hard load instead...`);
@@ -103,8 +125,9 @@ const handleTransition = async (targetUrl: string) => {
 };
 
 const navigateTo = async (targetUrl: string) => {
-  await handleTransition(targetUrl);
-  window.history.pushState(null, document.title, targetUrl);
+  // The final and target URLs can differ due to HTTP redirects.
+  const finalUrl = await handleTransition(targetUrl);
+  window.history.pushState(null, document.title, finalUrl || targetUrl);
   window.scrollTo(0, 0);
 };
 
