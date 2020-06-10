c479a9595ae162dac5a8d863bbbd1848b88ac913
Thomas Bonk <thomas@bonk.dev>
2020-06-10 16:34:51 +0200

    Output static markup

--- benchmarks/react/index.ts
+++ benchmarks/react/index.ts
@@ -1,6 +1,6 @@
-import { renderToNodeStream } from 'react-dom/server';
+import { renderToStaticNodeStream } from 'react-dom/server';
 import Page from './page';
 import getProducts from '../get-products';
 
-const executeBenchmark = async () => renderToNodeStream(Page({ products: await getProducts() }));
+const executeBenchmark = async () => renderToStaticNodeStream(Page({ products: await getProducts() }));
 export default executeBenchmark;
