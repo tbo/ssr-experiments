8c609d45942d206b5f15711f8429ddb9797cac64
Thomas Bonk <thomas@bonk.dev>
2020-06-20 15:09:37 +0200

    Add more test cases

--- tests/template-tag.ts
+++ tests/template-tag.ts
@@ -16,4 +16,39 @@ describe('Template tag', () => {
   test('Render simple template tag', async () => {
     await expectJSXtoMatchSnapshot(() => html`test`);
   });
+
+  test('Render simple template tag with number substitution', async () => {
+    await expectJSXtoMatchSnapshot(() => html`test${42}`);
+  });
+
+  test('Render async template tag ', async () => {
+    await expectJSXtoMatchSnapshot(async () => html`async test`);
+  });
+
+  test('Render embedded async string', async () => {
+    await expectJSXtoMatchSnapshot(() => html`${Promise.resolve('async')} test`);
+  });
+
+  test('Render embedded async number', async () => {
+    await expectJSXtoMatchSnapshot(() => html`async ${Promise.resolve(42)}`);
+  });
+
+  test('Render embedded template tag', async () => {
+    await expectJSXtoMatchSnapshot(() => html`another ${html`template tag`}`);
+  });
+
+  test('Render embedded async template tag', async () => {
+    await expectJSXtoMatchSnapshot(() => html`another async ${Promise.resolve(html`template tag`)}`);
+  });
+
+  test('stream template tag', async () => {
+    let trigger: any;
+    let output = '';
+    render(html`another async ${new Promise((resolve) => (trigger = resolve))}`).on('data', (data) => (output += data));
+    await nextTick();
+    expect(output).toMatchSnapshot();
+    trigger();
+    await nextTick();
+    expect(output).toMatchSnapshot();
+  });
 });
